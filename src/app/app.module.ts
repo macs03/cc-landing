b57eccc9df7b629534570df066c1c900f0b4f44e
Miguel Cardenas <miguelcarde.3@gmail.com>
2017-08-28 22:34:22 -0500

    Structure of landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import {
+  RouterModule,
+  Routes,
+  RouterOutlet,
+  RouterLink,
+  RouterLinkWithHref,
+  RouterLinkActive
+} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { LandingComponent } from "./components/landing/landing.component";
+import { ProveedoresComponent } from "./components/proveedores/proveedores.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/", pathMatch: "full" },
+  { path: "", component: LandingComponent },
+  { path: "proveedores", component: ProveedoresComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    ProveedoresComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/proveedores/proveedores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedores',
+  templateUrl: './proveedores.component.html',
+  styleUrls: ['./proveedores.component.sass']
+})
+export class ProveedoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
