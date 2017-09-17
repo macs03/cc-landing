8dccecd20b5fe5d5ccc2f36547c7d5e856868863
Miguel Cardenas <miguelcarde.3@gmail.com>
2017-09-17 17:14:39 -0500

    add form for providers

--- src/app/components/providers/providers.component.ts
+++ src/app/components/providers/providers.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-providers',
-  templateUrl: './providers.component.html',
-  styleUrls: ['./providers.component.scss']
+  selector: "app-providers",
+  templateUrl: "./providers.component.html",
+  styleUrls: ["./providers.component.scss"]
 })
 export class ProvidersComponent implements OnInit {
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+  codigo = "#Providers-VIP";
+  code: string = "";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.myStyle = {
+      position: "fixed",
+      width: "100%",
+      height: "100%",
+      "z-index": -2,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      "background-color": "#171300"
+    };
+
+    this.myParams = {
+      particles: {
+        number: {
+          value: 100
+        },
+        color: {
+          value: "#9b8001"
+        },
+        shape: {
+          type: "polygon"
+        }
+      }
+    };
   }
 
+  compareCode() {
+    console.log(this.code);
+    if (this.code === this.codigo) {
+      console.log("pasa");
+    }
+  }
 }
